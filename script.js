8fd704ce851f2c8d03e15ccb34adc2c4a7f359de
Arzoo Singh <arzoosingh147@gmail.com>
2024-08-17 02:23:48 +0530

    more progress

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const checkBoxList= document.querySelectorAll('.custom-checkbox')
+const checkBoxList = document.querySelectorAll('.custom-checkbox')
 const inputFields = document.querySelectorAll('.goal-input')
+const errorLabel = document.querySelector('.error-label')
+const progressBar = document.querySelector('.progress-bar')
+const progressValue = document.querySelector('.progress-value')
 
 checkBoxList.forEach((checkbox)=>{
    checkbox.addEventListener('click', (e)=> {
+
     checkbox.parentElement.classList.toggle('completed')
    })
 })
